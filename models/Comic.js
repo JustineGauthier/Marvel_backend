b833a9e6568c6189d513e712ce68c0f9249571eb
Justine Gauthier <gauthier.justine25000@gmail.com>
2024-06-23 20:26:59 +0200

    some littles rectifications

--- models/Comic.js
+++ models/Comic.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const comicSchema = new mongoose.Schema({
-  comic_title: { type: String, required: true },
-  comic_description: { type: String },
-  comic_image: { type: Object },
-  characters: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Character",
-    },
-  ],
-});
+// const comicSchema = new mongoose.Schema({
+//   comic_title: { type: String, required: true },
+//   comic_description: { type: String },
+//   comic_image: { type: Object },
+//   characters: [
+//     {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "Character",
+//     },
+//   ],
+// });
 
-const Comic = mongoose.model("Comic", comicSchema);
+// const Comic = mongoose.model("Comic", comicSchema);
 
-module.exports = Comic;
+// module.exports = Comic;
--- routes/character.js
+++ routes/character.js
@@ -15,38 +15,53 @@ router.get("/", async (req, res) => {
       characters = characters.filter((character) => regex.test(character.name));
     }
 
-    const limit = 5;
-    const pageNumber = page || 1;
+    const limit = 8;
+    const pageNumber = parseInt(page) || 1;
     const numberToSkip = (pageNumber - 1) * limit;
-    const paginatedCharacters = characters.slice(
-      numberToSkip,
-      numberToSkip + limit
-    );
+    const paginatedData = characters.slice(numberToSkip, numberToSkip + limit);
+    const totalPages = Math.ceil(characters.length / limit);
 
-    res.status(200).json(paginatedCharacters);
+    res.status(200).json({ paginatedData, limit, pageNumber, totalPages });
   } catch (error) {
     console.log("error", error);
     res.status(500).json({ message: error.message });
   }
 });
 
-router.get("/:id", async (req, res) => {
-  try {
-    const character_id = req.params.id;
+// router.get("/:id", async (req, res) => {
+//   try {
+//     const character_id = req.params.id;
 
-    let apiUrl = `https://lereacteur-marvel-api.herokuapp.com/character/${character_id}?apiKey=${process.env.MARVEL_API_KEY}`;
-    const response = await axios.get(apiUrl);
-    let character = response.data;
+//     let apiUrl = `https://lereacteur-marvel-api.herokuapp.com/character/${character_id}?apiKey=${process.env.MARVEL_API_KEY}`;
+//     const response = await axios.get(apiUrl);
+//     let character = response.data;
 
-    if (!character) {
-      return res.status(404).json({ message: "Ce personnage n'existe pas" });
-    }
+//     if (!character) {
+//       return res.status(404).json({ message: "Ce personnage n'existe pas" });
+//     }
+//     // console.log("character1", character);
 
-    res.status(200).json(character);
-  } catch (error) {
-    console.log("error", error);
-    res.status(500).json({ message: error.message });
-  }
-});
+//     const comicsPromises = character.comics.map(async (comic) => {
+//       const comicResponse = await axios.get(
+//         `https://lereacteur-marvel-api.herokuapp.com/comic/${comic}?apiKey=${process.env.MARVEL_API_KEY}`
+//       );
+//       return {
+//         _id: comicResponse.data._id,
+//         title: comicResponse.data.title,
+//         thumbnail: comicResponse.data.thumbnail,
+//       };
+//     });
+
+//     const comicsDetails = await Promise.all(comicsPromises);
+//     character.comics = comicsDetails;
+
+//     // console.log("character2", character);
+
+//     res.status(200).json(character);
+//   } catch (error) {
+//     console.log("error", error);
+//     res.status(500).json({ message: error.message });
+//   }
+// });
 
 module.exports = router;
